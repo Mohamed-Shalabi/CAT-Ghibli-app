9c2f488fbb7e77f1e1f56447111d2927d260b55d
Mohamed-Shalabi <mshalaby10@gmail.com>
2022-08-16 15:38:42 +0200

    resetted all anime view model code

--- lib/modules/all_anime/view_models/all_anime_view_model.dart
+++ lib/modules/all_anime/view_models/all_anime_view_model.dart
@@ -1,26 +1,3 @@
 import 'package:flutter/material.dart';
-import 'package:state_management_session_two/models/anime_model.dart';
-import 'package:state_management_session_two/modules/all_anime/all_anime_repository.dart';
 
-class AllAnimeViewModel with ChangeNotifier {
-  var allAnimeFilms = <AnimeModel>[];
-  var isLoading = false;
-
-  Future<bool> getAllAnimeFilms() async {
-    isLoading = true;
-    notifyListeners();
-
-    final result = await AllAnimeRepository.parseAllAnime();
-
-    isLoading = false;
-
-    if (result.statusCode == 200) {
-      allAnimeFilms = result.data ?? [];
-      notifyListeners();
-      return true;
-    } else {
-      notifyListeners();
-      return false;
-    }
-  }
-}
+class AllAnimeViewModel with ChangeNotifier {}
